d745d42b341567dfb34a3c3e655750eff6fdb28d
Yuki <yuki_matsubara@outlook.com>
2021-02-22 00:58:28 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/* Yuki Matsubara - Morning Class WMAD2 Week1 Weekly Assignment */
+/* ===== Move to top button =====*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 200) {
+      $('#toTop').fadeIn();
+  } else {
+      $('#toTop').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#toTop").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+});
+
+/*Examples shown in the articles */
+// //closure
+// function outerFunction () {
+//   const outer = `I see the outer variable!`
+
+//   function innerFunction() {
+//     console.log("I am here " + outer);
+//   }
+//   return innerFunction;
+// }
+// outerFunction()();
+
+  
+
+
+
+
+
+
